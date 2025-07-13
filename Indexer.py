719de6d7f7fe49eee8d20f682004862310b08ca0
michaelrzg <michaelrizig@gmail.com>
2025-07-13 01:05:51 -0400

    added lookup table, display results now works

--- Indexer.py
+++ Indexer.py
@@ -27,6 +27,7 @@ class Indexer:
         self.dataset = {}
         self.tok2idx = defaultdict(lambda: len(self.tok2idx))
         self.idx2tok = dict()
+        self.lookup_table = {}
         # load config data
         self.data_directory = cfg['data_dir']
         self.index_file = cfg["idx_file"]
@@ -37,15 +38,17 @@ class Indexer:
                 with open(self.index_file,"rb") as file:
                     self.inverted_index = pickle.load(file)
                 print("index loaded")
+
             except Exception as e:
                 print(e)
         else:
-            print("index file not found, generating inv index")
+            print("index not found, recreating")
+
             # generate the inverted index from data
             self.generate_inverted_index()
             # save 
-            self.save_inv_index()
-    
+            self.save()
+        self.avglen = self.lookup_table['avglen']
     def load_corups(self,dir):
         current_id = 0
         total_length = 0
@@ -56,18 +59,18 @@ class Indexer:
                 path = os.path.join(dir,file) # get path to each file
                 with open(path) as current:
                     full_text = current.read()
-                    full_text = self.get_text(full_text)# get data undrer TExt: header
-                    page = self.text_preprocessing(full_text)
+                    text_section = self.get_text(full_text)# get data undrer TExt: header
+                    page = self.text_preprocessing(text_section)
                     total_length+=len(page)
                     self.dataset[current_id] = page
+                    self.lookup_table[current_id] = {"title": file,"url":self.get_url(full_text),"text" : text_section}
                     current_id+=1
                     self.add_to_vocabulary(page)
-            self.avglen = total_length/N
-                    
-
+            self.lookup_table['avglen'] =total_length/N
+            self.lookup_table['N'] = N
         except Exception as e:
             print(e)
-        print(len(self.tok2idx))
+
 
     def get_text(self,full_file):
         start_index = full_file.find("Text: ") # find the Text header
@@ -75,7 +78,10 @@ class Indexer:
             text_content_start = start_index + len("Text: ") 
             extracted_text = full_file[text_content_start:].strip() # grab the rest of the file after the header
             return extracted_text
-
+    def get_url(self,full_file):
+        line = full_file.splitlines()[0].split(": ")[1]
+        return line
+    
     def text_preprocessing(self, text):
         text = text.lower() # lowercase
         tokens = word_tokenize(text) #tokenize
@@ -112,7 +118,7 @@ class Indexer:
                     if not found_doc:
                         self.inverted_index[word].append((doc_id, freq)) # else add it as a new document frequency paor
 
-    def save_inv_index(self):
+    def save(self):
         with open(self.index_file,"wb") as file:
             pickle.dump(self.inverted_index,file)
 
@@ -121,7 +127,7 @@ class SearchAgent:
         self.indexer = indexer
         self.k1 = cfg['k1']
         self.b = cfg['b']
-        self.N = len(indexer.dataset)
+        self.N = indexer.lookup_table['N']
         self.inv = indexer.inverted_index
 
     def query(self, q_str):
@@ -130,7 +136,6 @@ class SearchAgent:
         for docID,text in self.indexer.dataset.items():
             bm25 = 0
             d = len(text)
-            print(query)
             for t in query:
                 if t not in self.inv:
                     continue 
@@ -151,16 +156,23 @@ class SearchAgent:
                 second_half = (term_freq * (self.k1 +1))/ (term_freq+self.k1 * (1 - self.b + (self.b * (d/self.indexer.avglen))))
 
                 bm25 += firsthalf * second_half
-            scores.append((docID,bm25))
+            if bm25 > 0:
+                scores.append((docID,bm25))
         scores.sort(key=lambda x: x[1], reverse=True)
-        return scores[:5]
+        self.display_results(scores)
+        return scores
 
 
-    def calculate_bm25(self):
-        pass
-
-    def display_results(self, results):
-        pass
+    def display_results(self, scores):
+        for index,x in enumerate(scores):
+            data = self.indexer.lookup_table[x[0]]
+            print("Rank: ", index+1)
+            print("DocID: ",x[0])
+            print("Score: ", x[1])
+            print('url: ', data['url'])
+            print('filename: ', data['title'])
+            print("\n\n")
+            
 
 CFG = {
     'idx_file': 'irdataset.idx',
